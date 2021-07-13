c3bc330486adc1c24d867a7272a7e7221e2a11f4
DogpaChen <dogpa0130@gmail.com>
2021-07-13 13:57:28 +0800

    Initial Commit

---
+++ Swift Practice # 32 test for action outlet tset/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 32 test for action outlet tset
+//
+//  Created by CHEN PEIHAO on 2021/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
